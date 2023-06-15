145cea6261970a89d575d7e874cf889ac7bed73e
shaiaviv <shaizxzx@gmail.com>
2023-06-15 19:15:50 +0300

    links between sign in and signup

--- app/src/main/java/com/example/chatapplication/MainActivity.java
+++ app/src/main/java/com/example/chatapplication/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.chatapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView txtAlreadyHave = findViewById(R.id.txtAlreadyHave);
+        txtAlreadyHave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Define the intent to launch the target activity
+                Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+
+                // Start the target activity
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/chatapplication/SignInActivity.java
+++ app/src/main/java/com/example/chatapplication/SignInActivity.java
@@ -2,7 +2,10 @@ package com.example.chatapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        TextView txtAlreadyHave = findViewById(R.id.txtClickSignUp);
+        txtAlreadyHave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Define the intent to launch the target activity
+                Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+
+                // Start the target activity
+                startActivity(intent);
+            }
+        });
     }
 }
