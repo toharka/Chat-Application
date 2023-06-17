8334a26788f436947d4ad4b4142c5ff9d749800c
shaiaviv <shaizxzx@gmail.com>
2023-06-17 19:20:29 +0300

    immediate validation only

--- app/src/main/java/com/example/chatapplication/MainActivity.java
+++ app/src/main/java/com/example/chatapplication/MainActivity.java
@@ -8,8 +8,11 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
 
@@ -29,6 +32,30 @@ public class MainActivity extends AppCompatActivity {
 
                 // Start the target activity
                 startActivity(intent);
+            }
+        });
+
+        EditText txtUsername = findViewById(R.id.txtUsername);
+        EditText txtPassword = findViewById(R.id.txtPassword);
+        EditText txtPasswordConf = findViewById(R.id.txtPasswordConf);
+        EditText txtNicName = findViewById(R.id.txtNicName);
+        Button btnSignup = findViewById(R.id.btnSignup);
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (txtUsername.getText().toString().isEmpty() ||
+                        txtNicName.getText().toString().isEmpty() ||
+                        txtPassword.getText().toString().isEmpty() ||
+                        txtPasswordConf.getText().toString().isEmpty()) {
+                    Toast.makeText(MainActivity.this, "invalid input", Toast.LENGTH_SHORT).show();
+                } else if (!txtPassword.getText().toString()
+                        .equals(txtPasswordConf.getText().toString())) {
+                    Toast.makeText(MainActivity.this, "password confirmation unsuccessful", Toast.LENGTH_SHORT).show();
+                }
+//                else if () {
+//                    //user exist
+//                }
             }
         });
 
--- app/src/main/java/com/example/chatapplication/SignInActivity.java
+++ app/src/main/java/com/example/chatapplication/SignInActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -23,7 +26,25 @@ public class SignInActivity extends AppCompatActivity {
 
                 // Start the target activity
                 startActivity(intent);
+
             }
         });
+
+        EditText txtUsername = findViewById(R.id.txtUsername);
+        EditText txtPassword= findViewById(R.id.txtPassword);
+        Button btnSignin = findViewById(R.id.btnSignin);
+
+        btnSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (txtUsername.getText().toString().isEmpty() ||
+                        txtPassword.getText().toString().isEmpty()) {
+                    Toast.makeText(SignInActivity.this, "invalid input", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+        });
+
     }
 }
