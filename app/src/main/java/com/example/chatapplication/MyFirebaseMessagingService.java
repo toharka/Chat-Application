615470cf70aa948027a7c1b343459e1695f5363e
toharka <toharkaner@gmail.com>
2023-06-24 02:28:42 +0300

    get notification

--- app/src/main/java/com/example/chatapplication/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/chatapplication/MyFirebaseMessagingService.java
@@ -8,9 +8,19 @@ import android.os.Build;
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
+import androidx.room.Room;
 
+import com.example.chatapplication.models.AppDB;
+import com.example.chatapplication.models.MessageDao;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Map;
+
+import models.Message;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
@@ -23,7 +33,6 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     public void onMessageReceived(RemoteMessage remoteMessage) {
 
         if (remoteMessage.getNotification() != null) {
-
             createNotificationChannel();
 
             NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "1")
@@ -35,16 +44,40 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
             NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
 
             if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
                 return;
             }
             notificationManager.notify(1, builder.build());
+
+            // Check if message contains a data payload.
+            if (remoteMessage.getData().size() > 0) {
+                String chatIdStr = remoteMessage.getData().get("chatId");
+                String messageIdStr = remoteMessage.getData().get("messageId");
+                String sender = remoteMessage.getData().get("sender");
+                String content = remoteMessage.getData().get("content");
+
+                // parse the chatId and messageId into integers
+                int chatId = Integer.parseInt(chatIdStr);
+                int messageId = Integer.parseInt(messageIdStr);
+
+                // create a new message object
+                Message message = new Message();
+                message.setId(messageId);
+                message.setChatId(chatId);
+                message.setSenderUsername(sender);
+                message.setContent(content);
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.getDefault());
+                String currentDateAndTime = sdf.format(new Date());
+                message.setCreated(currentDateAndTime);
+                message.setCreated(new Date().toString());  // adjust if necessary
+                message.setReceived(true);  // this is a received message
+
+                // add the new message to your local database
+                new Thread(() -> {
+                    AppDB appDB = Room.databaseBuilder(getApplicationContext(), AppDB.class, "database-name").build();
+                    MessageDao messageDao = appDB.messageDao();
+                    messageDao.insertMessage(message);
+                }).start();
+            }
         }
     }
 
